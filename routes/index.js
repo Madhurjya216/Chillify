95c3fcc25057ce75e0c545885b50af5f3d3cfcb2
unknown <madhurjyakashyap216@gmail.com>
2024-03-11 10:40:48 -0500

    further commited

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.get("/signup", function (req, res, next) {
+  res.render("signup");
+});
+
+// router.post("/signup", function (req, res, next) {
+//   try {
+//   } catch (error) {
+//     console.error("Server error >>>", error);
+//   }
+// });
 
 module.exports = router;
